09ec47aa313ee25da92af9ae0b464e30310c9681
Reinaldo <reinaldoper83@gmail.com>
2022-10-26 15:12:40 -0300

    ajustando requisito 7

--- desafio1.sql
+++ desafio1.sql
@@ -123,17 +123,17 @@ DROP DATABASE IF EXISTS SpotifyClone;
   INSERT INTO SpotifyClone.Seguindo (usuario_id, artista_id_seguindo)
  VALUES (1, 1),
  (1, 2),
- (1, 4),
+ (1, 3),
  (2, 1),
- (2, 4),
+ (2, 3),
  (3, 2),
- (4, 6),
- (5, 3),
+ (4, 4),
  (5, 5),
- (6, 5),
+ (5, 6),
+ (6, 6),
  (6, 1),
- (7, 5),
- (9, 4),
+ (7, 6),
+ (9, 3),
  (10, 2);
  
  
